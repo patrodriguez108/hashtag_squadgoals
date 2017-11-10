31eb61d8cd084b135d56215a70677ebb7a9c0a38
Patrick <pat.rodriguez108@gmail.com>
2017-11-10 15:42:15 -0600

    Able to create a new vision

--- app/controllers/visions_controller.rb
+++ app/controllers/visions_controller.rb
@@ -4,7 +4,12 @@ class VisionsController < ApplicationController
 	end
 
 	def create
+		vision = Vision.new(statement: params[:vision][:statement], timeframe: Date.new(params[:vision][:timeframe].to_i + Date.today.year, Date.today.month, Date.today.day), user_id: current_user.id)
+		if vision.save
+			redirect_to "/users/#{current_user.id}"
+		else
 
+		end
 	end
 
 end
