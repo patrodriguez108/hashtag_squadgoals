7967d6ab2bd022998a4b66ee2df230da775a8003
Patrick <pat.rodriguez108@gmail.com>
2017-11-10 14:47:20 -0600

    Able to create a goal; move new goal form into partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,13 @@ class ApplicationController < ActionController::Base
   def about
   end
 
-   before_action :configure_permitted_parameters, :only => [:create, :edit]
+  #  before_action :configure_permitted_parameters, :only => [:create, :edit]
 
-  protected
+  # protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do |user|
-        user.permit(:username, :email, :password, :given_name, :family_name)
-      end
-  end
+  #   def configure_permitted_parameters
+  #     devise_parameter_sanitizer.permit(:sign_up) do |user|
+  #       user.permit(:username, :email, :password, :given_name, :family_name)
+  #     end
+  # end
 end
