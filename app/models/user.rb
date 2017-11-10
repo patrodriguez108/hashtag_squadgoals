7f8ecf607152534b0fbc204457acce7bbe5c3b3d
kristinmac <kristin.macdonough@gmail.com>
2017-11-09 20:04:25 -0600

    Updated associations between user and connections, created associations in goal and tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   has_many :goals
   has_many :visions
-  has_many :squad_members, class_name: 'Connection'
-  has_many :champs, class_name: 'Connection'
+
+  has_many :champ_connections, class_name: :Connection, foreign_key: :champ_id
+  has_many :squad_members, through: :champ_connections
+
+  has_many :squad_connections, class_name: :Connection, foreign_key: :squad_member_id
+  has_many :champs, through: :squad_connections
 end
