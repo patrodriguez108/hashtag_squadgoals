bc395420280088dd42c086ac1a543955d87a5e59
kristinmac <kristin.macdonough@gmail.com>
2017-11-10 16:52:09 -0600

    Add list of champs to user show page, define tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-	
+
   def show
     @user = User.find(params[:id])
+    p @champ = @user.champs.last
+    p "Champ's last goal content"
+    p @champ.goals.last.content
   end
 end
