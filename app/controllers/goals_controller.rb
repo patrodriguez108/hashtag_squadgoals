b349793a47e46886b8b1a41d5d8e0a61f97d28d1
Patrick <pat.rodriguez108@gmail.com>
2017-11-10 17:42:13 -0600

    Able to convert tags in params into array

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,8 +5,9 @@ class GoalsController < ApplicationController
 	end
 
 	def create
-		p params
+		p params[:tag]
 		goal = Goal.new(content: params[:goal][:content], by_when: Date.new(params[:goal][:by_when].to_i, Date.today.month, Date.today.day), user_id: current_user.id, category_id: params[:goal][:category_id].to_i)
+		# goal.tags << 
 		# if goal.save
 			if params[:private].to_i == 1
 				goal.private = true
