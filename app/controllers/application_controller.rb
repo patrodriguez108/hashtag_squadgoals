9cdc92b84e19a029d1a902ff765f1f098e247155
kristinmac <kristin.macdonough@gmail.com>
2017-11-10 12:00:56 -0600

    Defined user registration and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
 
   def index
   end
+
+   before_action :configure_permitted_parameters, :only => [:create, :edit]
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user|
+        user.permit(:username, :email, :password, :given_name, :family_name)
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "application#index"
   devise_for :users
+
+  resources :users, only: [:show]
 end
