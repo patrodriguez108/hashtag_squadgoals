c5945ae4bfdcac074f77d101a02d745ded93ae78
Patrick <pat.rodriguez108@gmail.com>
2017-11-12 12:41:06 -0600

    Add first three user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,13 +7,33 @@ describe User do
   	User.create(given_name: "Georgi", family_name: "Wiesel", username: "i_m_wiesel", email: "g.wiesel@email.org", password: "password")
   	User.create(given_name: "Frederick", family_name: "Wiesel", username: "f_m_wiesel", email: "f.wiesel@email.org", password: "password")
   	User.create(given_name: "Ronald", family_name: "Wiesel", username: "ron_wiesel", email: "r.wiesel@email.org", password: "password")
+  	Category.create(name: "Health")
  		Goal.create(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
 		Goal.create(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
 		Goal.create(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
   end
 
+  let(:users) { User.all }
+
+  let(:goals) { Goal.all }
+
+  let(:first_user) { User.first }
+
   xit 'returns a collection of users' do
-  	# p User.all.class
-  	expect(User.all.class).to eq(User::ActiveRecord_Relation)
+  	expect(users.class).to eq(User::ActiveRecord_Relation)
+  end
+
+  describe "user's associations" do
+  	it 'has goals' do
+  		expect(first_user.goals).to eq goals
+  	end
+
+  	it 'includes each goal' do
+  		expect(first_user.goals.include?(Goal.first)).to be true
+  		expect(first_user.goals.include?(Goal.second)).to be true
+  		expect(first_user.goals.include?(Goal.third)).to be true
+
+  	end
+
   end
 end
