2509b7963496fb9225c20b4222661270ddf30aa0
Patrick <pat.rodriguez108@gmail.com>
2017-11-12 12:47:29 -0600

    Add path for user to create a new vision and a new goal after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user.save
       p "WTF"
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to new_vision_path
     else
       render 'new'
     end
--- app/controllers/visions_controller.rb
+++ app/controllers/visions_controller.rb
@@ -6,7 +6,7 @@ class VisionsController < ApplicationController
 	def create
 		vision = Vision.new(statement: params[:vision][:statement], timeframe: Date.new(params[:vision][:timeframe].to_i + Date.today.year, Date.today.month, Date.today.day), user_id: current_user.id)
 		if vision.save
-			redirect_to "/users/#{current_user.id}"
+			redirect_to new_goal_path
 		else
 
 		end
