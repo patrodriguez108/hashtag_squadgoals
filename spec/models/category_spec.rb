5cde61f4bfeb43515b922650ebc60bc5fd4a11e6
Patrick <pat.rodriguez108@gmail.com>
2017-11-12 10:45:06 -0600

    Add describe block for category association with goal

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,18 +1,40 @@
 require 'rails_helper'
 
 describe Category do
+	let(:category) { Category.new }
+
 	before(:each) do 
 		Category.create(name: "Health")
 		Category.create(name: "Career")
 		Category.create(name: "Personal")
+		User.create(given_name: "Georgi", family_name: "Wiesel", username: "i_m_wiesel", email: "g.wiesel@email.org", password: "password")
+		Goal.create(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
+		Goal.create(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
+		Goal.create(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
 	end
 
-	let(:health) { Category.first }
-	let(:career) { Category.second }
-	let(:personal) { Category.third }
+	let(:categories) { Category.all }
+
+	let(:health_category) { Category.first }
+	let(:career_category) { Category.second }
+	let(:personal_category) { Category.third }
+
 
 	it 'has three categories' do
-		expect(Category.all.count).to eq 3
+		expect(categories.count).to eq 3
+	end
+
+	xit 'returns a collection of categories' do
+		expect(categories.class).to eq(Category::ActiveRecord_Relation)
+	end
+
+	describe 'catergory's association with goals' do
+
+		xit 'has a collection of goals' do
+			expect(health_category.goals.class).to eq(Goal::ActiveRecord_Associations_CollectionProxy)
+		end
+
+		
 	end
 
 end
