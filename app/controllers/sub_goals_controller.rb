00b8981e3fcd4c8cf160f867383b3491f9f47220
kristinmac <kristin.macdonough@gmail.com>
2017-11-12 10:19:39 -0600

    modify sub goals controller

--- app/controllers/sub_goals_controller.rb
+++ app/controllers/sub_goals_controller.rb
@@ -1,5 +1,8 @@
 class SubGoalsController < ApplicationController
 
+  def index
+    @sub_goals = SubGoal.all
+  end
 
   def new
     @goal = Goal.find(params[:goal_id])
@@ -7,12 +10,10 @@ class SubGoalsController < ApplicationController
 
   def create
     @goal = Goal.find(params[:goal_id])
+    # Add logic to check the by_when of the goal vs the by_when of the sub_goal
     @sub_goal = @goal.sub_goals.create(params[:sub_goal].permit(:content, :by_when))
     redirect_to goal_path(@goal)
   end
 
-  def index
-    @goal = Goal.find(params[:goal_id])
-  end
 
 end
