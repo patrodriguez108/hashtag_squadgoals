37ff2ee1f9fa4920631bfdf4269168f6960825cf
Patrick <pat.rodriguez108@gmail.com>
2017-11-12 14:51:03 -0600

    Add initial logic for ajaxing first new goal upon user registration

--- app/controllers/visions_controller.rb
+++ app/controllers/visions_controller.rb
@@ -6,9 +6,13 @@ class VisionsController < ApplicationController
 	def create
 		vision = Vision.new(statement: params[:vision][:statement], timeframe: Date.new(params[:vision][:timeframe].to_i + Date.today.year, Date.today.month, Date.today.day), user_id: current_user.id)
 		if vision.save
-			respond_to do |f|
-				f.html { redirect_to new_goal_path }
-				f.js
+			if current_user.visions.length > 1
+				redirect_to user_path(current_user.id)
+			else
+				respond_to do |f|
+					f.html { redirect_to new_goal_path }
+					f.js
+				end
 			end
 		else
 
