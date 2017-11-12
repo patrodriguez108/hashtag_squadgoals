afe01b6dda7fb67ab7218df70f3e12757e90582c
kristinmac <kristin.macdonough@gmail.com>
2017-11-12 14:52:17 -0600

    Begin checkbox

--- app/controllers/sub_goals_controller.rb
+++ app/controllers/sub_goals_controller.rb
@@ -11,8 +11,14 @@ class SubGoalsController < ApplicationController
   def create
     @goal = Goal.find(params[:goal_id])
     # Add logic to check the by_when of the goal vs the by_when of the sub_goal
-    @sub_goal = @goal.sub_goals.create(params[:sub_goal].permit(:content, :by_when))
-    redirect_to goal_path(@goal)
+    @sub_goal = @goal.sub_goals.new(params[:sub_goal].permit(:content, :by_when))
+    if @sub_goal.save
+      redirect_to goal_path(@goal)
+    end
+  end
+
+  def update
+
   end
 
 
