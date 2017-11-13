131200eccef97e27cae4cea74a33cad6c28c0899
austinratcliff <austin.ratcliff@icloud.com>
2017-11-13 14:47:35 -0600

    Getting there with cronofy I think

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,9 +37,17 @@ class UsersController < ApplicationController
     render
   end
 
-  # def cronofy
-  #   render json: params.to_json
-  # end
+  def cronofy
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to user_path(@user.id)
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 
   private
 
