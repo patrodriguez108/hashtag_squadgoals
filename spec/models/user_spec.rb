914e9663bcffb7e1b612df54fe2d5a6eb7ce1a8e
Patrick <pat.rodriguez108@gmail.com>
2017-11-13 08:57:45 -0600

    Pass pending tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,31 +8,34 @@ describe User do
   	User.create(given_name: "Frederick", family_name: "Wiesel", username: "f_m_wiesel", email: "f.wiesel@email.org", password: "password")
   	User.create(given_name: "Ronald", family_name: "Wiesel", username: "ron_wiesel", email: "r.wiesel@email.org", password: "password")
   	Category.create(name: "Health")
- 		Goal.create(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
-		Goal.create(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
-		Goal.create(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
+    first_goal = Goal.new(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
+    first_goal.tags << Tag.create(name: "body goals")
+    first_goal.save
+    second_goal = Goal.new(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
+    second_goal.tags << Tag.create(name: "dieting")
+    second_goal.save
+    third_goal = Goal.new(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
+    third_goal.tags << Tag.create(name: "exercising")
+    third_goal.save
   end
 
   let(:users) { User.all }
 
-  let(:goals) { Goal.all }
-
   let(:first_user) { User.first }
 
-  xit 'returns a collection of users' do
-  	expect(users.class).to eq(User::ActiveRecord_Relation)
+  it 'returns a collection of users' do
+    users.each { |user| expect(user).to be_instance_of(User) }
   end
 
   describe "user's associations" do
-  	xit 'has goals' do
-  		expect(first_user.goals).to eq goals
+  	it 'has goals' do
+  		first_user.goals.each { |goal| expect(goal).to be_instance_of(Goal) }
   	end
 
-  	xit 'includes each goal' do
+  	it 'includes each goal' do
   		expect(first_user.goals.include?(Goal.first)).to be true
   		expect(first_user.goals.include?(Goal.second)).to be true
   		expect(first_user.goals.include?(Goal.third)).to be true
-
   	end
 
   end
