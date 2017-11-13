3b905c9d7464bb9b652966e9c76aedf89916ed0e
Patrick <pat.rodriguez108@gmail.com>
2017-11-12 18:05:28 -0600

    Remove pseudocode

--- app/controllers/sub_goals_controller.rb
+++ app/controllers/sub_goals_controller.rb
@@ -10,10 +10,7 @@ class SubGoalsController < ApplicationController
 
   def create
     @goal = Goal.find(params[:goal_id])
-    # Add logic to check the by_when of the goal vs the by_when of the sub_goal
     @sub_goal = @goal.sub_goals.create(params[:sub_goal].permit(:content, :by_when))
     redirect_to goal_path(@goal)
   end
-
-
 end
