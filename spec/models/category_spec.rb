b799be75b53a1d835d03f5fb7d885213f58117c4
Patrick <pat.rodriguez108@gmail.com>
2017-11-13 08:45:17 -0600

    Pass pending tests for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe Category do
-	let(:category) { Category.new }
-
 	before(:each) do 
 		Category.create(name: "Health")
 		Category.create(name: "Career")
 		Category.create(name: "Personal")
 		User.create(given_name: "Georgi", family_name: "Wiesel", username: "i_m_wiesel", email: "g.wiesel@email.org", password: "password")
-		Goal.create(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
-		Goal.create(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
-		Goal.create(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
+		first_goal = Goal.new(content: "I will have ripped abs", by_when: Date.new(2020, 2, 20), category_id: Category.first.id, user_id: User.first.id)
+		first_goal.tags << Tag.create(name: "body goals")
+		first_goal.save
+		second_goal = Goal.new(content: "I will stop eating a whole box of cookies", by_when: Date.new(2019, 4, 20), category_id: Category.first.id, user_id: User.first.id)
+		second_goal.tags << Tag.create(name: "dieting")
+		second_goal.save
+		third_goal = Goal.new(content: "I will start exercising more often", by_when: Date.new(2018, 1, 20), category_id: Category.first.id, user_id: User.first.id)
+		third_goal.tags << Tag.create(name: "exercising")
+		third_goal.save
 	end
 
 	let(:categories) { Category.all }
@@ -19,27 +23,23 @@ describe Category do
 	let(:career_category) { Category.second }
 	let(:personal_category) { Category.third }
 
-
-	xit 'has three categories' do
+	it 'has three categories' do
 		expect(categories.count).to eq 3
 	end
 
-	xit 'returns a collection of categories' do
-		expect(categories.class).to eq(Category::ActiveRecord_Relation)
+	it 'returns a collection of categories' do
+		categories.each { |catergory| expect(category).to be_instance_of(Category) }
 	end
 
 	describe "catergory's association with goals" do
-
-		xit 'has a collection of goals' do
-			expect(health_category.goals.class).to eq(Goal::ActiveRecord_Associations_CollectionProxy)
+		it 'has a collection of goals' do
+			health_category.goals.each { |goal| expect(goal).to be_instance_of(Goal) } 
 		end
 
-		xit 'includes each goal' do
+		it 'includes each goal' do
 			expect(health_category.goals.include?(Goal.first)).to be true
 			expect(health_category.goals.include?(Goal.second)).to be true
 			expect(health_category.goals.include?(Goal.third)).to be true
 		end
-
 	end
-
 end
