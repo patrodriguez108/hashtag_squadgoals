23ebeffb1c0edc079398f624b274668d3a4a0e4c
Patrick <pat.rodriguez108@gmail.com>
2017-11-13 14:52:43 -0600

    Default project name and content to empty strings

--- db/migrate/20171113175816_create_projects.rb
+++ db/migrate/20171113175816_create_projects.rb
@@ -1,8 +1,8 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
-      t.string :name
-      t.text :description
+      t.string :name, default: ""
+      t.text :description, default: ""
 
       t.timestamps
     end
