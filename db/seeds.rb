ab7ff447e9d97986abaeee931f00c357d12897f9
Patrick <pat.rodriguez108@gmail.com>
2017-11-14 09:24:23 -0600

    Update seeds file to reflect change of Collaboration

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,6 +68,5 @@ end
 	project = Project.find(rand(1..10))
 	collaborator_one = User.find(rand(1..5))
 	collaborator_two = User.find(rand(6..10))
-	collab_one = Collaboration.create(status_id: 2, project: project, collaborator: collaborator_one, sent_request: true)
-	collab_two = Collaboration.create(status_id: 2, project: project, collaborator: collaborator_two)
+	Collaboration.create(status_id: 2, project: project, request_sender: collaborator_one, request_receiver: collaborator_two)
 end
