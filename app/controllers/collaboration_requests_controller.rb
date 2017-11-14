b159ad15bee15ea1c95ba243fe3eeb6998588c2b
Patrick <pat.rodriguez108@gmail.com>
2017-11-14 11:55:43 -0600

    Enable user to send request and accept request to collaborate

--- app/controllers/collaboration_requests_controller.rb
+++ app/controllers/collaboration_requests_controller.rb
@@ -1,7 +1,38 @@
 class CollaborationRequestsController < ApplicationController
 
-	def new
+	def index
 		@collaboration_requests = CollaborationRequest.where(request_receiver: current_user, status_id: 1)
 	end
 
+	def new
+		@user = User.find(params[:user_id])
+	end
+
+	def show
+		@collaboration_request = CollaborationRequest.find_by(id: params[:id], request_sender_id: params[:user_id])
+	end
+
+	def create
+		collaboration_request = CollaborationRequest.new(request_receiver_id: params[:user_id], request_sender: current_user)
+		collaboration_request.status = RequestStatus.find(1)
+
+		collaboration_request.save
+
+		redirect_to user_path(current_user.id)
+	end
+
+	def accept
+		collaboration_request = CollaborationRequest.find_by(id: params[:id], request_receiver_id: current_user.id, request_sender_id: params[:user_id])
+		collaboration_request.status = RequestStatus.find(2)
+		collaboration_request.save
+
+		project = Project.new
+
+		receiver_collaboration = Collaboration.new(project: project, collaborator: collaboration_request.request_receiver, request: collaboration_request)
+		sender_collaboration = Collaboration.new(project: project, collaborator: collaboration_request.request_sender, request: collaboration_request)
+
+		receiver_collaboration.save && sender_collaboration.save
+
+		redirect_to user_path(current_user.id)
+	end
 end
--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -1,53 +1,53 @@
 class CollaborationsController < ApplicationController
 
 	def index
-		@collaborations = current_user.collaborations.where(status_id: 1)
+		# @collaborations = current_user.collaborations.where(status_id: 1)
 	end
 
 	def new
-		@user = User.find(params[:user_id])
+		# @user = User.find(params[:user_id])
 	end
 
 	def show
-		@current_user_collaboration = Collaboration.find(params[:id])
+		# @current_user_collaboration = Collaboration.find(params[:id])
 	end
 
 	def create
 		project = Project.create
 
-		current_user_collaboration = Collaboration.new(
-			collaborator_id: current_user.id, 
-			project: project, sent_request: true)
-		current_user_collaboration.status = RequestStatus.find(1)
+		# current_user_collaboration = Collaboration.new(
+		# 	collaborator_id: current_user.id, 
+		# 	project: project, sent_request: true)
+		# current_user_collaboration.status = RequestStatus.find(1)
 
-		requested_collaboration = Collaboration.create(
-			collaborator_id: params[:user_id], 
-			project: project)
-		requested_collaboration.status = RequestStatus.find(1)
+		# requested_collaboration = Collaboration.create(
+		# 	collaborator_id: params[:user_id], 
+		# 	project: project)
+		# requested_collaboration.status = RequestStatus.find(1)
 
-		current_user_collaboration.save && requested_collaboration.save
+		# current_user_collaboration.save && requested_collaboration.save
 		
-		redirect_to user_path(current_user.id)
+		# redirect_to user_path(current_user.id)
 	end
 
 	def accept
-		requested_collaboration = Collaboration.find_by(collaborator_id: params[:user_id], status_id: 1, sent_request: true)
-		requested_collaboration.status_id = 2
-		requested_collaboration.save
+		# requested_collaboration = Collaboration.find_by(collaborator_id: params[:user_id], status_id: 1, sent_request: true)
+		# requested_collaboration.status_id = 2
+		# requested_collaboration.save
 
-		current_user_collaboration = Collaboration.find_by(collaborator_id: current_user.id, status_id: 1, project: requested_collaboration.project)
-		current_user_collaboration.status_id = 2
-		current_user_collaboration.save
+		# current_user_collaboration = Collaboration.find_by(collaborator_id: current_user.id, status_id: 1, project: requested_collaboration.project)
+		# current_user_collaboration.status_id = 2
+		# current_user_collaboration.save
 
-		redirect_to projects_path
+		# redirect_to projects_path
 	end
 
 	def decline
-		requested_collaboration = Collaboration.find_by(collaborator_id: params[:user_id], status_id: 1, sent_request: true)
+		# requested_collaboration = Collaboration.find_by(collaborator_id: params[:user_id], status_id: 1, sent_request: true)
 		# requested_collaboration.status_id = 3
 		# requested_collaboration.save
 
-		current_user_collaboration = Collaboration.find_by(collaborator_id: current_user.id, status_id: 1, project: requested_collaboration.project)
+		# current_user_collaboration = Collaboration.find_by(collaborator_id: current_user.id, status_id: 1, project: requested_collaboration.project)
 		# current_user_collaboration.status_id = 3
 		# current_user_collaboration.save
 
