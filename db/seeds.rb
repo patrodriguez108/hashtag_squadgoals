e0b38c45a64ed0c670cdf94d8bc262cb4cd2f746
kristinmac <kristin.macdonough@gmail.com>
2017-11-15 15:06:25 -0600

    Ready to reseed to test profile pic uploads

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require 'faker'
 
+profile_pic_samples = ["construction-worker_emoji", "female-artist-type-6"]
+
 10.times do
 	user = User.new(
 		given_name: Faker::Name.first_name,
@@ -8,6 +10,12 @@ require 'faker'
 	user.username = Faker::Internet.user_name(user.given_name)
 	user.email = Faker::Internet.safe_email("#{user.given_name} #{user.family_name}")
 	user.password = "password"
+
+	sample = profile_pic_samples.sample
+
+	image_path = File.open("/app/assets/images/seed/#{sample}.png")
+
+	user.profile_pic = image_path
 	user.save
 
 	Vision.create(
@@ -16,6 +24,8 @@ require 'faker'
 		user_id: user.id
 		)
 end
+
+
 
 Category.create(name: "Health")
 Category.create(name: "Career")
@@ -39,30 +49,67 @@ Tag.create(name: "volunteering")
 
 personal_goal_content = ["Travel to Spain","Climb on a glacier in Alaska","Learn to windsurf","Learn to Scubadive","Build an art collection","Publish a Short Story","Master 5 magic tricks"]
 
-personal_goal_subgoals = ["Write for a full 12 hours", "Buy a how-to book", "Take a class", "Meet new people who do what I enjoy", "Create a performance for my family", "Visit the museum", "Ignore mom's advice"]
+personal_goal_subgoals = ["Write for a full 12 hours", "Buy a how-to book", "Take a class", "Meet new people who do what I enjoy", "Create a performance for my family", "Visit the museum", "Ignore mom's advice", "Stop buying lottery tickets", ]
 
 
 health_goal_content = ["Run a 10K","Run a mile in under 8 minutes","Attend yoga three times a week","Develop a nutrition plan","Learn to box","Try new vegetables","Learn to cook 20 new dishes"]
 
-health_goal_subgoals = ["Sign up for a race", "Learn to make more salads", "Take a cooking class", "Buy a cook book", "Get tips from Aunt Tilda", ""]
+health_goal_subgoals = ["Sign up for a race", "Learn to make more salads", "Take a cooking class", "Buy a cook book", "Get tips from Aunt Tilda", "Throw out all the candy"]
 
 
 professional_goal_content = ["Apply to new jobs","Complete a leadership training course","Publish in a professional journal","Mentor a junior developer","Start a management position in 4 years","Start my MBA","Start a new business"]
 
 professional_goal_subgoals = ["Make a new contact", "Apply for a course", "Put in an application", "Get a loan", "Attend a networking event", "Build a resume", "Buy a how to book", "Make an awkward phone call", "Ignore dad's advice"]
 
-
-
-
-20.times do
-	goal = Goal.new(content: Faker::HarryPotter.quote, by_when: Faker::Date.between(Date.today, 10.years.from_now), private: false, user_id: rand(1..10), category_id: rand(1..3))
+# HEALTH GOALS
+10.times do
+	goal = Goal.new(content: health_goal_content.sample, by_when: Faker::Date.between(Date.today, 10.years.from_now), private: false, user_id: rand(1..10), category_id: 1)
 
 	5.times do
 		goal.tags << Tag.find(rand(1..15))
 	end
 
 	goal.save
+
+	rand(1..4).times do
+		g.sub_goals << SubGoal.create(content: health_goal_subgoals.sample, goal_id: g.id, complete: false)
+	end
+
 end
+
+#CAREER GOALS
+10.times do
+	goal = Goal.new(content: professional_goal_content.sample, by_when: Faker::Date.between(Date.today, 10.years.from_now), private: false, user_id: rand(1..10), category_id: 2)
+
+	5.times do
+		goal.tags << Tag.find(rand(1..15))
+	end
+
+	goal.save
+
+	rand(1..4).times do
+		g.sub_goals << SubGoal.create(content: professional_goal_subgoals.sample, goal_id: g.id, complete: false)
+	end
+
+end
+
+#PERSONAL GOALS
+10.times do
+	goal = Goal.new(content: personal_goal_content.sample, by_when: Faker::Date.between(Date.today, 10.years.from_now), private: false, user_id: rand(1..10), category_id: 3)
+
+	5.times do
+		goal.tags << Tag.find(rand(1..15))
+	end
+
+	goal.save
+
+	rand(1..4).times do
+		g.sub_goals << SubGoal.create(content: personal_goal_subgoals.sample, goal_id: g.id, complete: false)
+	end
+
+end
+
+
 
 RequestStatus.create(name: "Requested")
 RequestStatus.create(name: "Accepted")
