ae89559e2552aa424685b74c57769e750e8468a8
austinratcliff <austin.ratcliff@icloud.com>
2017-11-15 13:11:12 -0600

    Reconfig production mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,6 +115,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
+    :domain               => 'squadgoals.herokuapp.com',
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
